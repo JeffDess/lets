15d2a602849bdb13188f47e8c04f1a643b3a6a3a
Jean-François Dessureault <6100619+JeffDess@users.noreply.github.com>
2026-06-29 20:16:01 -0400

    feat: support darwin/aarch64 systems

--- flake.nix
+++ flake.nix
@@ -21,10 +21,15 @@
       flake-parts,
     }:
     let
-      system = "x86_64-linux";
       version = "0.1.0";
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems f;
+
       letsLib = import ./lib;
       inherit (letsLib)
         mkTask
@@ -35,46 +40,102 @@
         mkBaseTasks
         mkFlake
         ;
-      tasks = mkBaseTasks { inherit pkgs; };
-      taskOutputs = mkOutputs { inherit pkgs tasks; };
-      letsCmd = mkLets { inherit pkgs tasks version; };
+
+      perSystem =
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tasks = mkBaseTasks { inherit pkgs; };
+          taskOutputs = mkOutputs { inherit pkgs tasks; };
+          letsCmd = mkLets { inherit pkgs tasks version; };
+          preCommitCheck = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              convco.enable = true;
+              deadnix = {
+                enable = true;
+                settings.edit = true;
+              };
+              editorconfig-checker.enable = true;
+              nixfmt.enable = true;
+              shellcheck = {
+                enable = true;
+                excludes = [ "\\.zsh$" ];
+              };
+              shfmt = {
+                enable = true;
+                settings.indent = 2;
+                excludes = [ "\\.zsh$" ];
+              };
+              statix.enable = true;
+              typos.enable = true;
+            };
+          };
+        in
+        {
+          inherit
+            pkgs
+            taskOutputs
+            letsCmd
+            preCommitCheck
+            ;
+        };
+
+      built = forAllSystems perSystem;
     in
     {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          packages = [ letsCmd ];
-          shellHook = ''
-            ${shellHook}
-            __lets_bash_version="''${BASH_VERSION:-}"
-            if [ -n "$__lets_bash_version" ] && type complete >/dev/null 2>&1; then
-              # shellcheck disable=SC1091
-              source ${taskOutputs.completionScripts.bash}
-            fi
-          '';
-          buildInputs = enabledPackages;
-        };
-      };
-
-      apps.${system} = taskOutputs.apps;
-
-      packages.${system} = {
-        default = letsCmd;
-        lets = letsCmd;
-        completions =
-          pkgs.runCommand "lets-completions"
-            {
-              nativeBuildInputs = [ pkgs.installShellFiles ];
-            }
-            ''
-              # shellcheck disable=SC2154
-              installShellCompletion --cmd lets \
-                --bash ${./completions/lets.bash} \
-                --zsh ${./completions/lets.zsh} \
-                --fish ${./completions/lets.fish}
-              install -Dm644 ${./completions/lets.nu} \
-                "$out/share/lets/completions/lets.nu"
+      devShells = forAllSystems (
+        system:
+        let
+          inherit (built.${system})
+            pkgs
+            taskOutputs
+            letsCmd
+            preCommitCheck
+            ;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ letsCmd ];
+            shellHook = ''
+              ${preCommitCheck.shellHook}
+              __lets_bash_version="''${BASH_VERSION:-}"
+              if [ -n "$__lets_bash_version" ] && type complete >/dev/null 2>&1; then
+                # shellcheck disable=SC1091
+                source ${taskOutputs.completionScripts.bash}
+              fi
             '';
-      };
+            buildInputs = preCommitCheck.enabledPackages;
+          };
+        }
+      );
+
+      apps = forAllSystems (system: built.${system}.taskOutputs.apps);
+
+      packages = forAllSystems (
+        system:
+        let
+          inherit (built.${system}) pkgs letsCmd;
+        in
+        {
+          default = letsCmd;
+          lets = letsCmd;
+          completions =
+            pkgs.runCommand "lets-completions"
+              {
+                nativeBuildInputs = [ pkgs.installShellFiles ];
+              }
+              ''
+                # shellcheck disable=SC2154
+                installShellCompletion --cmd lets \
+                  --bash ${./completions/lets.bash} \
+                  --zsh ${./completions/lets.zsh} \
+                  --fish ${./completions/lets.fish}
+                install -Dm644 ${./completions/lets.nu} \
+                  "$out/share/lets/completions/lets.nu"
+              '';
+        }
+      );
 
       lib = {
         inherit
@@ -90,42 +151,26 @@
 
       flakeModules.default = letsLib.flakeModule version;
 
-      formatter.${system} = pkgs.nixfmt-tree;
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
 
-      checks.${system} = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            convco.enable = true;
-            deadnix = {
-              enable = true;
-              settings.edit = true;
-            };
-            editorconfig-checker.enable = true;
-            nixfmt.enable = true;
-            shellcheck = {
-              enable = true;
-              excludes = [ "\\.zsh$" ];
-            };
-            shfmt = {
-              enable = true;
-              settings.indent = 2;
-              excludes = [ "\\.zsh$" ];
-            };
-            statix.enable = true;
-            typos.enable = true;
-          };
-        };
-      }
-      // import ./checks {
-        inherit
-          pkgs
-          system
-          self
-          nixpkgs
-          flake-parts
-          taskOutputs
-          ;
-      };
+      checks = forAllSystems (
+        system:
+        let
+          inherit (built.${system}) pkgs taskOutputs preCommitCheck;
+        in
+        {
+          pre-commit-check = preCommitCheck;
+        }
+        // import ./checks {
+          inherit
+            pkgs
+            system
+            self
+            nixpkgs
+            flake-parts
+            taskOutputs
+            ;
+        }
+      );
     };
 }
